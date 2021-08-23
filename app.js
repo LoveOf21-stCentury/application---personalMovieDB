7ef941abad171b6a27f73bb87790b561dfee322c
Lyubov <l.musilmanbekova12@gmail.com>
2021-08-23 09:41:24 +0300

    index.html mod and more automatizated system of DB

--- app.js
+++ app.js
@@ -8,13 +8,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('one of them?', ''),
-      b = prompt('did you liked it?', ''),
-      c = prompt('one of them?', ''),
-      d = prompt('did you liked it?', '');
-    
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('one of last seen movie?', ''),
+          b = prompt('rate how much did you liked it?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        alert('done');
+    } else {
+        alert ('you did something wrong');
+        i--; 
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert ('Hmm... not much') 
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert ('You kind of love it')
+} else if(personalMovieDB.count >= 30) {
+    alert ('Wow you are really love it')
+} else {
+    alert ('something goes wrong')
+}
 
 console.log(personalMovieDB);
 
