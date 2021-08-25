0a998b4adf5a5fe5be8ccb446eeb1c24abbbac04
2e2 <e3.r33e@2eev.com>
2021-08-25 17:15:09 +0300

    add function(start(), rememberMyFilms(), detectPersonalLevel(), showMyDB(.privat)

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const numberOfFilms = +prompt('сколько фильмов посмотрели');
-``
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('сколько фильмов посмотрели?', '');
+    while (numberOfFilms === '' || numberOfFilms === null || isNaN(numberOfFilms)); {
+        numberOfFilms = +prompt('сколько фильмов посмотрели?', '');
+    }
+}
+
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,29 +17,45 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('one of last seen movie?', ''),
-          b = prompt('rate how much did you liked it?', '');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('one of last seen movie?', ''),
+              b = prompt('rate how much did you liked it?', '');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        alert('done');
-    } else {
-        alert ('you did something wrong');
-        i--; 
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            alert('done');
+        } else {
+            alert ('you did something wrong');
+            i--; 
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    alert ('Hmm... not much') 
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert ('You kind of love it')
-} else if(personalMovieDB.count >= 30) {
-    alert ('Wow you are really love it')
-} else {
-    alert ('something goes wrong')
+//rememberMyFilms();
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Hmm... not much') ; 
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('You kind of love it'); 
+    } else if(personalMovieDB.count >= 30) {
+        console.log('Wow you are really love it'); 
+    } else {
+        console.log('something goes wrong'); 
+    }
 }
 
-console.log(personalMovieDB);
+//detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
 
 
